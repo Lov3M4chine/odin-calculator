a0e54d583eeb5e1c3c72ced669becf11b2d71eda
Lov3M4chine <125694577+Lov3M4chine@users.noreply.github.com>
2023-04-06 16:16:39 -0600

    Add display test message and general functions for calculator operations

---
+++ scripts.js
@@ -0,0 +1,25 @@
+let num1 = 0;
+let num2 = 0;
+
+//on first button press sequence, store number into num1
+//end first button press sequence when operation is pressed
+//begin second button press sequence, store number into num2
+//end second button press sequence when operation is pressed or equal sign is pressed
+
+//addition function
+function operationAddition {
+    return num1 + num2;
+}
+//subtraction function
+function operationSubtration {
+    return num1 - num2;
+}
+//multiplication function
+function operationMultiplication {
+    return num1 * num2;
+}
+//division function
+function operationDivision {
+    return num1 / num2;
+}
+//save to memory function
